6f7c3070b7357ec7d6f9e30c935177af8a0488de
Sai Gudigundla <saip106@msn.com>
2015-02-17 19:09:58 -0500

    added negative spec for zero sum checker

--- src/tests/zero-sum-checker-spec.js
+++ src/tests/zero-sum-checker-spec.js
@@ -16,4 +16,14 @@ describe('when checking for zero sum', function () {
         expect(result).toBe(true);
     });
 
+    it('should find if a matrix does not have a zero sum', function () {
+        var matrix = new Matrix(2, 2);
+        matrix.set(0,0,1);
+        matrix.set(0,1,2);
+        matrix.set(1,1,21);
+        matrix.set(1,0,10);
+
+        var result = zeroSumChecker.check(matrix);
+        expect(result).toBe(false);
+    });
 });
